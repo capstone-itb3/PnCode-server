74bf362c1a00f51560f8212e6fbbc0817c13990d
jromarllegue <lleguejohnromar02@gmail.com>
2024-04-16 11:33:47 +0800

    5000

--- index.js
+++ index.js
@@ -59,5 +59,5 @@ io.on('connection', (socket) =>  {
 
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
