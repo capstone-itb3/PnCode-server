213caa963313d8c5ba4d5fa8138d338d019f22d4
jromarllegue <lleguejohnromar02@gmail.com>
2024-09-05 21:10:54 +0800

    client suddenly getting blocked by cors

--- index.js
+++ index.js
@@ -44,6 +44,27 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+});
+
 app.use(accountRouter);
 app.use(roomRouter);
 app.use(teamRouter);
@@ -51,7 +72,7 @@ app.use(activityRouter);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://codlin-client.onrender.com",
+        origin: "*",
         // origin: "http://localhost:5173",
         methods: ["GET", "POST"]
     }
