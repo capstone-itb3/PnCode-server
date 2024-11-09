73980708db81e0aefb8c8d30e7d29d2dcf6a25c7
jromarllegue <lleguejohnromar02@gmail.com>
2024-11-09 22:40:06 +0800

    solo room output render

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const socketConnect = require('./socket');
 const PORT = process.env.PORT || 5000;
 
 const fileModel = require('./models/files.model');
+const soloRoomModel = require('./models/solo_rooms.model');
 
 app.use(cors());
 app.use(express.json());
@@ -113,3 +114,50 @@ app.get('/view/:room_id/:file_name', async (req, res) => {
     return res.status(500).json({ status: false, message: 'Server error. Retrieving file failed.' });
   }
 })
+
+app.get('/view/solo/:room_id/:file_name', async (req, res) => {
+  try {
+    console.log(req.params.room_id);
+    console.log(req.params.file_name);
+
+
+    const room = await soloRoomModel.findOne({ room_id: String(req.params.room_id) })
+                 .select('files owner_id')
+                 .lean();
+    if (!room) {
+      return res.status(404).send('Room not found.');
+    };
+
+    const files = room.files;
+    const file = files.find(f => f.name === req.params.file_name);
+
+    if (!file) {
+      return res.status(404).send('File not found.');
+    }
+
+    // if (room.owner_id === req.user.uid) {
+    //     return res.status(200).json({   status: 'ok',
+    //                                     files: files,
+    //                                     active: file,
+    //                                     message: 'Files retrieved successfully.' });
+    // } else {
+    //     return res.status(403).json({ status: false, message: 'Not a part of this room.'});
+    // }
+    console.log('aaa');
+
+    const type = () => {
+      if (file.type === 'html') return 'text/html';
+      else if (file.type === 'js') return 'text/javascript';
+      else if (file.type === 'css') return 'text/css';
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Content-Type', type());
+
+    return res.send(file.content);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ status: false, message: 'Server error. Retrieving file failed.' });
+  }
+});
